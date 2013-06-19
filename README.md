ba70992362473af62c35c154ec1182ac96c18183
VirtualSkynet <alandxxx@gmail.com>
2013-06-18 21:57:49 -0500

    10:00 18/06/2013

--- README.md
+++
@@ -1,7 +0,0 @@
-VirtualSkynet Installer
-========================
-
-
-1. wget https://github.com/virtualskynet/install/archive/master.tar.gz
-2. cd install-master
-3. sh install.sh
--- clientpack/install.sh
+++ clientpack/install.sh
@@ -1,16 +1,18 @@
 
 SKYNETDIR="/root/virtualskynet"
-CLIENT="dialcloud"
+CLIENT="myclient"
+
 
 mv $SKYNETDIR/webapps/virtualskynet.war $SKYNETDIR/webapps/$CLIENT.war
 
 sleep 10
 
-rm -rf $SKYNETDIR/webapps/$CLIENT/zimages/background.jpg
-rm -rf $SKYNETDIR/webapps/$CLIENT/zimages/logo.png
-rm -rf $SKYNETDIR/webapps/$CLIENT/zimages/logo2.png
 
-cp background.jpg $SKYNETDIR/webapps/$CLIENT/zimages
-cp logo.png $SKYNETDIR/webapps/$CLIENT/zimages
-cp logo2.png $SKYNETDIR/webapps/$CLIENT/zimages
+rm -rf $SKYNETDIR/webapps/$CLIENT/images/background.jpg
+rm -rf $SKYNETDIR/webapps/$CLIENT/images/logo.png
+
+
+cp background.jpg $SKYNETDIR/webapps/$CLIENT/images
+cp logo.png $SKYNETDIR/webapps/$CLIENT/images
+
 
